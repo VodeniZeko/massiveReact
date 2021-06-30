406a6b9af14cec9d8a1133ed75024c4fbe86c60f
VodeniZeko <edvin.saletovic@pm.me>
2021-06-30 01:22:14 -0700

    fixes, lazy load, typos

--- src/components/ui/ContentHero.js
+++ src/components/ui/ContentHero.js
@@ -1,52 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
+import classNames from "classnames";
 
 import seniorHippster from "../../assets/senior.jpg";
 import doctor from "../../assets/young-doctor.jpg";
 
 const ContentHero = () => {
+  const [zip, setZip] = useState(" ");
+
+  const handleInput = (event) => {
+    setZip(event.target.value);
+  };
+
+  const submit = () => {
+    setZip("");
+  };
   return (
     <>
       <div className="w-full max-w-screen-xl ml-1 lg:flex lg:flex-row ">
         <div>
           <img
-            class="bg-center bg-cover"
+            className="bg-center bg-cover"
             src={seniorHippster}
             alt="senior hippster"
           />
         </div>
 
-        <div class="flex justify-end -m-8 md:m-0 sm:w-screen lg:w-11/12 lg:mr-10 max-w-10/12">
-          <div class="container z-40 w-3/5 mx-auto lg:mt-14 lg:w-11/12">
-            <form class="text-white bg-blue-700 rounded-lg shadow-md ">
-              <div class="flex justify-center bg-blue-500 rounded-t-lg h-14 sm:h-28">
-                <span class="self-center font-semibold text-md sm:text-2xl xl:text-4xl">
-                  Find Mediare Coverage
+        <div className="flex justify-end -m-8 md:m-0 sm:w-screen lg:w-11/12 lg:mr-10 max-w-10/12">
+          <div className="container z-40 w-3/5 mx-auto lg:mt-14 lg:w-11/12">
+            <form className="text-white bg-blue-700 rounded-lg shadow-md ">
+              <div className="flex justify-center bg-blue-500 rounded-t-lg h-14 sm:h-28">
+                <span className="self-center font-semibold text-md sm:text-2xl xl:text-4xl">
+                  Find Medicare Coverage
                 </span>
               </div>
 
-              <div class="flex flex-col items-center">
-                <div class="flex flex-col items-center py-4 text-sm sm:text-lg xl:text-2xl">
+              <div className="flex flex-col items-center">
+                <div className="flex flex-col items-center py-4 text-sm sm:text-lg xl:text-2xl">
                   <p>
-                    Compare <span class="font-bold">Medicare Plans</span>
+                    Compare <span className="font-bold">Medicare Plans</span>
                   </p>
                   <p>in one minute!</p>
                 </div>
 
-                <div class="flex flex-col items-center w-10/12 pt-3 pb-4 mb-6 bg-white rounded-lg sm:pt-6 sm:pb-8 sm:mb-12">
+                <div className="flex flex-col items-center w-10/12 pt-3 pb-4 mb-6 bg-white rounded-lg sm:pt-6 sm:pb-8 sm:mb-12">
                   <label
                     for="something"
-                    class="block mb-2 text-xs font-medium text-blue-700 sm:text-sm xl:text-md"
+                    className="block mb-2 text-xs font-medium text-blue-700 sm:text-sm xl:text-md"
                   >
                     Enter Your Zip Code
                   </label>
                   <input
+                    maxLength={5}
+                    onChange={handleInput}
                     type="text"
-                    placeholder=""
-                    class="w-11/12 h-10 py-2 text-xl text-center text-gray-700 border-2 border-black rounded appearance-none xl:h-14 focus:outline-none focus:bg-white"
+                    value={zip}
+                    placeholder={isNaN(zip) && "number only"}
+                    className={classNames(
+                      "w-11/12 h-10 py-2 text-xl text-center text-gray-700 border-2 border-black rounded appearance-none xl:h-14 focus:outline-none focus:bg-white",
+                      {
+                        "text-red-600 font-bold": isNaN(zip),
+                      }
+                    )}
                   />
 
                   <button
-                    class="w-3/5 py-3 mt-4 text-xs font-bold text-white bg-yellow-600 rounded shadow sm:text-sm sm:w-9/12 lg:text-lg xl:text-3xl hover:bg-yellow-500 focus:shadow-outline focus:outline-none"
+                    onClick={submit}
+                    className="w-3/5 py-3 mt-4 text-xs font-bold text-white bg-yellow-600 rounded shadow sm:text-sm sm:w-9/12 lg:text-lg xl:text-3xl hover:bg-yellow-500 focus:shadow-outline focus:outline-none"
                     type="button"
                   >
                     Get Started
@@ -56,45 +75,21 @@ const ContentHero = () => {
             </form>
           </div>
         </div>
-
-        {/* <section className="bg-red-700">
-        <div class="relative w-full max-w-screen-xl ml-1 -m-20 bg-blue-100 h-36 md:h-72">
-          <img
-            alt="doctor"
-            class="inset-0 z-0 w-full bg-center bg-cover h-36 md:h-72 opacity-40"
-            src={doctor}
-          />
-
-          <div class="absolute inset-0 z-10 flex items-center justify-center">
-            <div class="flex w-full px-24 md:flex-col">
-              <p class="mb-4 text-sm font-semibold text-blue-800 opacity-0 md:text-4xl md:opacity-80 ">
-                Find & Compare Your Local <br /> Plans and Providers Fast
-              </p>
-              <p class="text-gray-500 opacity-0 text-md md:opacity-100">
-                {" "}
-                Let us help you find the right Medicare Supplement Coverage plan
-                for you. In less than <br /> 1 minute you could be saving your
-                deductibles, co-pays and Medicare bills
-              </p>
-            </div>
-          </div>
-        </div>
-      </section> */}
       </div>
 
-      <div class="relative w-full max-w-screen-xl ml-1 -m-20 bg-blue-100 h-36 md:h-72">
+      <div className="relative w-full max-w-screen-xl ml-1 -m-20 bg-blue-100 h-36 md:h-72">
         <img
           alt="doctor"
-          class="inset-0 z-0 w-full bg-center bg-cover h-36 md:h-72 opacity-40"
+          className="inset-0 z-0 w-full bg-center bg-cover h-36 md:h-72 opacity-40"
           src={doctor}
         />
 
-        <div class="absolute inset-0 z-10 flex items-center justify-center">
-          <div class="flex w-full px-24 md:flex-col">
-            <p class="mb-4 text-sm font-semibold text-blue-800 opacity-0 md:text-4xl md:opacity-80 ">
+        <div className="absolute inset-0 z-10 flex items-center justify-center">
+          <div className="flex w-full px-24 md:flex-col">
+            <p className="mb-4 text-sm font-semibold text-blue-800 opacity-0 md:text-4xl md:opacity-80 ">
               Find & Compare Your Local <br /> Plans and Providers Fast
             </p>
-            <p class="text-gray-500 opacity-0 text-md md:opacity-100">
+            <p className="text-gray-500 opacity-0 text-md md:opacity-100">
               {" "}
               Let us help you find the right Medicare Supplement Coverage plan
               for you. In less than <br /> 1 minute you could be saving your
--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { App } from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+import Loader from "./components/util/Loader";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const HomePage = lazy(() => import("./App"));
+
+function App() {
+  return (
+    <div className="App">
+      <Suspense maxDuration={300} fallback={<Loader />}>
+        <HomePage />
+      </Suspense>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
